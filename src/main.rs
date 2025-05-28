e768b35198a0aebce9ec447bacd38a69f6f67e8b
user <ddevil70707@gmail.com>
2025-05-28 22:25:09 +0545

    double-faults

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
 
-// This function is called a panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    println!("Hello world");
+
+    blog_os::init();
+
+    // x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("it did not crash..!");
+    #[allow(clippy::empty_loop)]
     loop {}
 }
